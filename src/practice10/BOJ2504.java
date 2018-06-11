dcd620baf514afc9980094e2136896c613376898
황선아 <tjsdk2769@gmail.com>
2018-06-11 22:36:40 +0900

    BOJ1026

---
+++ src/practice10/BOJ2504.java
@@ -0,0 +1,39 @@
+package practice10;
+import java.util.*;
+import java.io.*;
+public class BOJ2504 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+        Stack<Character> stack = new Stack<>();
+        int tmp=1;
+        int sum=0;
+        for(int i=0;i<s.length();i++) {
+			switch (s.charAt(i)){
+				case '(':
+					tmp*=2;
+					stack.push(s.charAt(i));
+					break;
+				case '[':
+					tmp*=3;
+					stack.push(s.charAt(i));
+					break;
+				case ')':
+					if (s.charAt(i-1) == '(') sum += tmp;
+					if (stack.empty()) return !printf("0");
+					if (stack. == '(') stack.pop();
+					tmp /= 2; break;
+					stack.pop();
+					break;
+				case ']':
+					stack.pop();
+					break;
+					
+			}
+		}
+		
+		
+		
+		
+	}
+}

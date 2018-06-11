6877ab8926b3dca2f738ad94004ec14b1e9d5ace
황선아 <tjsdk2769@gmail.com>
2018-06-11 23:08:55 +0900

    BOJ1024 수열의 합

--- src/practice13/BOJ1024.java
+++ src/practice13/BOJ1024.java
@@ -1,5 +1,13 @@
 package practice13;
-
+import java.util.*;
 public class BOJ1024 {
-
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int l = sc.nextInt();
+		if(l>100) System.out.println(-1);
+		else {
+			
+		}
+	}
 }

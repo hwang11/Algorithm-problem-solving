f7832dc1504af7ac1d3bbe0b604bae7405f710f3
황선아 <tjsdk2769@gmail.com>
2020-02-12 12:18:49 +0900

    백준 드래곤커브

--- src/BOJ/B15685.java
+++ src/BOJ/B15685.java
@@ -1,47 +1,69 @@
 package BOJ;
 
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class B15685 {
-	static int n;
+	static int N,ans;
+	static int dr[][] = {{1,0},{0,-1},{-1,0},{0,1}}; //x,y
 	static boolean v[][];
-	static int dr[][] = {{1,0},{0,-1},{-1,0},{0,1}}; //x,y 우 상 좌 하 
-	static void print() {
-		for(int i=0;i<10;i++) {
-			for(int j=0;j<10;j++) {
-				if(v[i][j]) System.out.print(1+"");
-				else System.out.print(0+"");
-			}
-			System.out.println();
-		}
-	}
-	static void dfs(int x,int y,int d,int g,int cnt) {
-		System.out.printf("x:%d y:%d\n",x,y);
+	static Stack<Integer> st[]; 
+	static LinkedList<Integer> li[];
+	static void draw(int x1,int y1,int d,int g, int idx) {
+		int x = x1;
+		int y = y1;
 		v[y][x] = true;
-		if(cnt == g+1) {
-			return;
-		}
+		li[idx].add(d);
+		st[idx].push(d);
 		int nx = x + dr[d][0];
 		int ny = y + dr[d][1];
-		
-		d--;
-		d+=4;
-		d%=4;
-		dfs(nx,ny,d,g,cnt+1);
-		
+		v[ny][nx] = true;
+		for(int i=0;i<g;i++) {
+			int size = st[idx].size();
+			for(int k=0;k<size;k++) {
+				int p = st[idx].pop();
+				int nd = (p + 1)%4;
+				nx = nx + dr[nd][0];
+				ny = ny + dr[nd][1];
+				v[ny][nx] = true;
+				li[idx].add(nd);
+			}
+			for(int j=0;j<li[idx].size();j++) {
+				st[idx].push(li[idx].get(j));
+			}
+
+		}
 	}
-	public static void main(String args[]) {
+	static boolean chk(int x,int y) { // 1x1 정사각형 확인 
+		if(!v[y][x] || !v[y][x+1] || !v[y+1][x] || !v[y+1][x+1]) return false;
+		return true;
+	}
+	static int count() {
+		int cnt = 0;
+		for(int i=0;i<100;i++) {
+			for(int j=0;j<100;j++) {
+				if(chk(i,j)) cnt++;
+			}
+		}
+		return cnt;
+	}
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		v = new boolean[101][101];
-		n = sc.nextInt();
-		for(int i=0;i<n;i++) {
+		N = sc.nextInt();
+		st = new Stack[N];
+		li = new LinkedList[N];
+		v = new boolean[101][101]; //x,y 범위가 100까지 였음. x,y <= 100 
+		for(int i=0;i<N;i++) {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
 			int d = sc.nextInt();
 			int g = sc.nextInt();
-			dfs(x,y,d,g,0);
-			print();
+			li[i] = new LinkedList<Integer>();
+			st[i] = new Stack<Integer>();
+			draw(x,y,d,g,i);
 		}
-		
+		ans = count();
+		System.out.println(ans);
 	}
 }
